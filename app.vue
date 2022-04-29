b299bd7e7a45ccb1efcd8159fd062575840fdf4a
Konstantin BIFERT <konstantin.bifert@gmail.com>
2022-04-29 10:54:51 +0200

    done

--- app.vue
+++ app.vue
@@ -1,9 +1,13 @@
 <script setup>
-import IconAccessibility from '~icons/carbon/accessibility'
+import IconXmark from '~icons/fa6-solid/xmark'
 import IconAccountBox from '~icons/mdi/account-box'
+import TastyPizza from '~icons/noto-v1/pizza'
+import IconPs from '~icons/ri/playstation-line'
 </script>
 
 <template>
-  <icon-accessibility/>
-  <icon-account-box style="font-size: 2em; color: red"/>
+  <icon-xmark style="font-size: 2em; color: blue" />
+  <icon-account-box style="font-size: 2em; color: red" />
+  <tasty-pizza style="font-size: 2em" />
+  <icon-ps style="font-size: 2em" />
 </template>
